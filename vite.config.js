7e59b9eea23f5357187bafc9d9467a327a4adaa4
shashi gharti <shashi.gharti@gmail.com>
2024-12-24 15:02:00 +0545

    Fixed icon font files link error

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   root: './src',
@@ -10,6 +11,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '@bootstrap-icons': path.resolve(__dirname, 'node_modules/bootstrap-icons')
     }
   },
   build: {
@@ -22,6 +25,18 @@ export default defineConfig({
         software: path.resolve(__dirname, 'src/software.html'),
         blogs: path.resolve(__dirname, 'src/blogs.html'),
       },
+      external: ['@bootstrap-icons/font/fonts/*'],
     },
   },
+  assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.eot', '**/*.ttf'],
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, 'node_modules/bootstrap-icons/font/fonts'),
+          dest: 'assets',
+        },
+      ],
+    }),
+  ],
 });
